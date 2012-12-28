8a3781897b1ee96e0cb0a9b6670ad4ec41e3ed68
Jeff French <jeff.french@moonswitch.com>
2012-12-28 12:28:22 -0600

    Renamed Flipper to FeatureFlipper
    Having class with the same name as the namespace forces external
    consumers to always reference the class as Flipper.Flipper().

--- Flipper.Tests/FeatureTests.cs
+++ Flipper.Tests/FeatureTests.cs
@@ -13,133 +13,133 @@ namespace Flipper.Tests
        [Test]
        public void Can_Activate_Feature()
        {
-           var feature = flipper.Get("Test");
+           var feature = FeatureFlipper.Get("Test");
 
-           flipper.ActivateFeature(feature);
+           FeatureFlipper.ActivateFeature(feature);
 
-           Assert.IsTrue(flipper.IsActive(feature));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature));
        }
 
        [Test]
        public void Can_Set_Active_Feature_To_Deactive()
        {
-           var feature = flipper.Get("Test");
+           var feature = FeatureFlipper.Get("Test");
 
-           flipper.ActivateFeature(feature);
+           FeatureFlipper.ActivateFeature(feature);
 
-           Assert.IsTrue(flipper.IsActive(feature));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature));
 
-           flipper.DeactiveFeature(feature);
+           FeatureFlipper.DeactiveFeature(feature);
 
-           Assert.IsFalse(flipper.IsActive(feature));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature));
        }
 
        [Test]
        public void Can_Define_A_Group()
        {
-           flipper.DefineGroup("test", new List<string>() { "1" });
+           FeatureFlipper.DefineGroup("test", new List<string>() { "1" });
 
-           Assert.IsTrue(flipper.GetGroup("test").Contains("1"));
+           Assert.IsTrue(FeatureFlipper.GetGroup("test").Contains("1"));
        }
 
 
        [Test]
        public void Can_Activate_A_Group()
        {
-           flipper.DefineGroup("test", new List<string>() { "1" });
+           FeatureFlipper.DefineGroup("test", new List<string>() { "1" });
 
-           var feature = flipper.Get("test");
+           var feature = FeatureFlipper.Get("test");
 
-           flipper.ActivateGroup(feature, "test");
+           FeatureFlipper.ActivateGroup(feature, "test");
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
        }
 
        [Test]
        public void Can_Deactivate_An_Activte_Group()
        {
-           flipper.DefineGroup("test", new List<string>() { "1" });
-           var feature = flipper.Get("test");
+           FeatureFlipper.DefineGroup("test", new List<string>() { "1" });
+           var feature = FeatureFlipper.Get("test");
 
-           flipper.ActivateGroup(feature, "test");
+           FeatureFlipper.ActivateGroup(feature, "test");
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
 
-           flipper.DeactivateGroup(feature, "test");
+           FeatureFlipper.DeactivateGroup(feature, "test");
 
-           Assert.IsFalse(flipper.IsActive(feature, "1"));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature, "1"));
        }
 
        [Test]
        public void Can_Activate_New_User_In_Feature()
        {
-           var feature = flipper.Get("Test");
+           var feature = FeatureFlipper.Get("Test");
 
-           flipper.ActivateUser(feature, "1");
+           FeatureFlipper.ActivateUser(feature, "1");
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
 
        }
 
        [Test]
        public void Can_Deactivate_A_User_In_A_Feature()
        {
-           var feature = flipper.Get("Test");
+           var feature = FeatureFlipper.Get("Test");
 
-           flipper.ActivateUser(feature, "1");
+           FeatureFlipper.ActivateUser(feature, "1");
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
 
-           flipper.DeactivateUser(feature, "1");
+           FeatureFlipper.DeactivateUser(feature, "1");
 
-           Assert.IsFalse(flipper.IsActive(feature, "1"));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature, "1"));
        }
 
        [Test]
        public void Can_Activate_A_Percentage()
        {
-           var feature = flipper.Get("Test");
+           var feature = FeatureFlipper.Get("Test");
            
-           flipper.ActivatePercentage(feature, 1);
+           FeatureFlipper.ActivatePercentage(feature, 1);
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
-           Assert.IsFalse(flipper.IsActive(feature, "2"));
-           Assert.IsFalse(flipper.IsActive(feature, "21"));
-           Assert.IsFalse(flipper.IsActive(feature, "31"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature, "2"));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature, "21"));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature, "31"));
 
-           flipper.ActivatePercentage(feature, 21);
+           FeatureFlipper.ActivatePercentage(feature, 21);
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
-           Assert.IsTrue(flipper.IsActive(feature, "102"));
-           Assert.IsTrue(flipper.IsActive(feature, "1021"));
-           Assert.IsFalse(flipper.IsActive(feature, "31"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "102"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1021"));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature, "31"));
 
        }
 
        [Test]
        public void Can_Deactivate_A_Percentage()
        {
-           var feature = flipper.Get("Test");
+           var feature = FeatureFlipper.Get("Test");
 
-           flipper.ActivatePercentage(feature, 1);
+           FeatureFlipper.ActivatePercentage(feature, 1);
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
 
-           flipper.DeactivatePercentage(feature);
+           FeatureFlipper.DeactivatePercentage(feature);
 
-           Assert.IsFalse(flipper.IsActive(feature, "1"));
+           Assert.IsFalse(FeatureFlipper.IsActive(feature, "1"));
        }
 
        [Test]
        public void Feature_Is_Always_Active_If_Percentage_Set_To_100()
        {
-           var feature = flipper.Get("Test");
+           var feature = FeatureFlipper.Get("Test");
 
-           flipper.ActivateFeature(feature);
+           FeatureFlipper.ActivateFeature(feature);
 
-           Assert.IsTrue(flipper.IsActive(feature, "1"));
-           Assert.IsTrue(flipper.IsActive(feature));
-           Assert.IsTrue(flipper.IsActive(feature, "12341"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "1"));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature));
+           Assert.IsTrue(FeatureFlipper.IsActive(feature, "12341"));
            
        }
    }
