7674f18355a233c11d7329935dca46e1a3e66bad
Michael Sarchet <mikesarchet@gmail.com>
2012-12-30 12:15:20 -0600

    Initial Admin Creationg
    This is currently just a view showing ul -> li

---
+++ Flipper.Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Flipper.Admin.Models;
+
+namespace Flipper.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.FlipperInfo = new FlipperInfo(new ServiceStack.Redis.BasicRedisClientManager("localhost:6379"));
+            return View();
+        }
+
+    }
+}
---
+++ Flipper.Admin/Models/FlipperInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.Redis;
+
+namespace Flipper.Admin.Models
+{
+    public class FlipperInfo
+    {
+        private IRedisClientsManager manager;
+
+        public List<Feature> Features { get; private set; }
+        public Dictionary<string, List<string>> Groups { get; private set; }
+
+        public FlipperInfo(IRedisClientsManager manager)
+        {
+            using (var client = manager.GetClient())
+            {
+                var fetaureRedis = client.As<Feature>();
+                Features = fetaureRedis.Lists["Flipper:Features"].ToList();
+
+                var groupRedis = client.As<List<string>>();
+                var hash = groupRedis.GetHash<string>("Flipper:Groups");
+                Groups = hash.GetAll();
+            }
+        }
+    }
+}
