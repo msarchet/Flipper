284736585281d3eb78039578280f4bac1689e416
Michael Sarchet <mikesarchet@gmail.com>
2013-03-09 09:17:36 -0600

    Add: Basic Percentage Funcs

---
+++ Flipper/PercentageFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Flipper
+{
+    public class PercentageFunctions
+    {
+        private static Random Randomizer = new Random(191872349861231);
+
+        public static Func<string, Feature, bool> DiceRoll = (user, feature) => { return (Randomizer.Next() % 100) <= feature.Percentage; };
+
+        public static Func<string, Feature, bool> UserNameHash = (user, feature) => { return (user.GetHashCode() % 100) <= feature.Percentage; };
+
+    }
+}
